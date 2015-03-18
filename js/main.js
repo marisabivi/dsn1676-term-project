ee2cc5c2ead6871c7c985cb95c5e9beb687ed16e
marisabivi <biv.marisa@yahoo.com>
2015-03-18 12:24:43 -0400

    woking on js and fixing start

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 var $showHide = $('.img-show-hide');
-var $box = $('.box');
+var $show = $('.show');
 
 var $btnMove = $('.img-move');
 
-$showHide.on('hover', function () {
-    $box.toggleClass('js-show');
+$showHide.ready(function () {
+    $show.hover('js-show');
 });
 
 $btnMove.on('click', function () {
     $diamond.toggleClass('js-move');
+});
+
+//$(document).ready(function(){
+    $("p").hover(function(){
+        $("p").css("background-color", "yellow");
+        },function(){
+        $("p").css("background-color", "pink");
+    });
 });
