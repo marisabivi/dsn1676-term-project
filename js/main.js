d8d755b53d32897eda4b5029bb3a05992932da33
marisabivi <biv.marisa@yahoo.com>
2015-03-18 19:04:07 -0400

    working on js for images

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 var $btnCollapse = $('.collapse');
 var $panel = $('.panel');
 
-var $showHide = $('.img-show-hide');
-var $show = $('.show');
-var $img1 = $('.img1-show');
-
-var $imgMove = $('.img1');
+var $thumbs = $('.thumbnails');
+var $bigImage = $('.big-img');
 
 $btnCollapse.on('click', function () {
     $panel.toggleClass('js-panel');
 });
 
-$imgMove.on('click', function () {
-    $img1.toggleClass('show1');
+$thumbs.on('click', 'a' function(e) {
+    e.preventDefault();
+    
 });
 
