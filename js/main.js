d598614d14e05991a5ae99bfb170923dd86cccc9
marisabivi <biv.marisa@yahoo.com>
2015-03-18 14:48:37 -0400

    working on button for clicking

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
+var $btnCollapse = $('.collapse');
+var $panel = $('.panel');
+
 var $showHide = $('.img-show-hide');
 var $show = $('.show');
 var $img1 = $('.img1-show')
 
 var $imgMove = $('.img1');
 
+$btnCollapse.on('click', function () {
+    $panel.toggleClass('js-panel');
+});
+
 $imgMove.on('click', function () {
     $img1.toggleClass('show1');
 });
+
