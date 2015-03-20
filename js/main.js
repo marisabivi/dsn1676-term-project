4b42adf955bdb5a9f7b17437283b29aeecf67922
marisabivi <biv.marisa@yahoo.com>
2015-03-20 17:15:49 -0400

    lightbox working and styled ul's

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ var $btnCollapse = $('.collapse');
 var $panel = $('.panel');
 
 var $thumbs = $('.thumbnails');
-var $bigImage = $('.big-img');
+var $lbImg = $('.lightbox-img')
+var $lb = $('.lightbox');
 
 $btnCollapse.on('click', function () {
     $panel.toggleClass('js-panel');
 });
 
-$thumbs.on('click', function (eventObject) {
-    eventObject.preventDefault();
+$thumbs.on('click', 'a', function (e) {
+    e.preventDefault();
+    var big = $(this).attr('href');
+    $lbImg.attr('src', big);
+    $lb.attr('data-state', 'visible') 
 });
 
