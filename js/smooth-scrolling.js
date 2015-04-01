d4c3ad1801ba1ec6549047114e447879abe4154c
marisabivi <biv.marisa@yahoo.com>
2015-04-01 18:49:37 -0400

    starting on scrolling

---
+++ js/smooth-scrolling.js
@@ -0,0 +1,22 @@
+var $btnCollapse = $('.collapse');
+var $panel = $('.panel');
+
+var $thumbs = $('.thumbnails');
+var $lbImg = $('.lightbox-img')
+var $lb = $('.lightbox');
+var $lbHeading = $('.lightbox-heading');
+var $lbDesc = $ ('.lb-desc');
+
+$btnCollapse.on('click', function () {
+    $panel.toggleClass('js-panel');
+});
+
+$thumbs.on('click', 'a', function (e) {
+    e.preventDefault();
+    var big = $(this).attr('href');
+    $lbImg.attr('src', big);
+    $lb.attr('data-state', 'visible');
+    $lbHeading.html($(this).attr('data-title'));
+    $lbDesc.html($(this).attr('data-desc'));
+});
+
